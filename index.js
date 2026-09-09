e900f60c7efd69bbee3fe00da3accc5b320f479e
changxianglin <281532612@qq.com>
2026-09-09 21:01:28 +0800

    middleware
    use router middleware

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const Koa = require('koa')
 const app = new Koa()
 
-app.use((ctx) => {
-    ctx.body = 'Hello Zhihu';
+app.use(async (ctx) => {
+    if(ctx.url === '/') {
+        ctx.body = 'This is Home'
+    } else if(ctx.url === '/users') {
+        if(ctx.method === 'GET') {
+            ctx.body = 'This users list'
+        } else if(ctx.method === 'POST') {
+            ctx.body = 'Create user'
+        } else {
+            ctx.status = 405
+        }
+    } else if(ctx.url.match(/\/users\/\w+/)) {
+        const userId = ctx.url.match(/\/users\/(\w+)/)[1]
+        ctx.body = `This userId ${userId}`
+    } else {
+        ctx.status = 404
+    }
 })
 
 app.listen(3000)
